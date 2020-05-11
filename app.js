38672abe83a8d7074983668d6a431927cea30781
Randeep Singh <randeeprattan90@gmail.com>
2020-05-11 20:24:54 +0100

    Change sounds feature

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
     this.playBtn = document.querySelector(".play");
+    this.currentKick = "./sounds/kick-classic.wav";
+    this.currentSnare = "./sounds/snare-acoustic01.wav";
+    this.currentHihat = "./sounds/hihat-acoustic01.wav";
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0; // track our postion in the track timeline
     this.bpm = 150;
+    this.isPlaying = null;
+    this.selects = document.querySelectorAll("select");
+    this.muteBtns = document.querySelectorAll(".mute");
   }
 
   activePad() {
@@ -42,15 +48,50 @@ class DrumKit {
   }
 
   start() {
-    console.log(this);
     const interval = (60 / this.bpm) * 1000;
-    setInterval(() => {
-      this.repeat();
-    }, interval);
+    // check if already playing
+    if (this.isPlaying) {
+      //clear interavl
+      clearInterval(this.isPlaying);
+      this.isPlaying = null;
+    } else {
+      this.isPlaying = setInterval(() => {
+        this.repeat();
+      }, interval);
+    }
+  }
+
+  updateButton() {
+    if (!this.isPlaying) {
+      this.playBtn.innerHTML = "Stop";
+      this.playBtn.classList.add("active");
+    } else {
+      this.playBtn.innerHTML = "Play";
+      this.playBtn.classList.remove("active");
+    }
+  }
+
+  changeSound(e) {
+    const selectionName = e.target.name;
+    const selectionValue = e.target.value;
+
+    switch (selectionName) {
+      case "kick-select":
+        this.kickAudio.src = selectionValue;
+        break;
+      case "snare-select":
+        this.snareAudio.src = selectionValue;
+        break;
+      case "hihat-select":
+        this.hihatAudio.src = selectionValue;
+        break;
+    }
   }
 }
 
 const drumKit = new DrumKit();
+
+//event listeners
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
@@ -60,5 +101,12 @@ drumKit.pads.forEach((pad) => {
 });
 
 drumKit.playBtn.addEventListener("click", function () {
+  drumKit.updateButton();
   drumKit.start();
+});
+
+drumKit.selects.forEach((select) => {
+  select.addEventListener("change", function (e) {
+    drumKit.changeSound(e);
+  });
 });
