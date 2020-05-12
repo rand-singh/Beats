fc68129508ff7e231dcec9a5c05780a4cf06bda6
Randeep Singh <randeeprattan90@gmail.com>
2020-05-12 20:10:45 +0100

    Added perc sound

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ class DrumKit {
     this.currentSnare = "./sounds/snare-acoustic01.wav";
     this.currentHihat = "./sounds/hihat-acoustic01.wav";
     this.currentClap = "./sounds/clap-808.wav";
+    this.currentPerc = "./sounds/perc-808.wav";
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.clapAudio = document.querySelector(".clap-sound");
+    this.percAudio = document.querySelector(".perc-sound");
     this.index = 0; // track our postion in the track timeline
     this.bpm = 150;
     this.isPlaying = null;
@@ -47,6 +49,10 @@ class DrumKit {
         if (bar.classList.contains("clap-pad")) {
           this.clapAudio.currentTime = 0;
           this.clapAudio.play();
+        }
+        if (bar.classList.contains("perc-pad")) {
+          this.percAudio.currentTime = 0;
+          this.percAudio.play();
         }
       }
     });
@@ -95,6 +101,9 @@ class DrumKit {
       case "clap-select":
         this.hihatAudio.src = selectionValue;
         break;
+      case "perc-select":
+        this.percAudio.src = selectionValue;
+        break;
     }
   }
 
@@ -112,8 +121,11 @@ class DrumKit {
         case "2":
           this.hihatAudio.volume = 0;
           break;
-        case "2":
+        case "3":
           this.clapAudio.volume = 0;
+          break;
+        case "4":
+          this.percAudio.volume = 0;
           break;
       }
     } else {
@@ -129,6 +141,9 @@ class DrumKit {
           break;
         case "3":
           this.clapAudio.volume = 1;
+          break;
+        case "4":
+          this.percAudio.volume = 1;
           break;
       }
     }
